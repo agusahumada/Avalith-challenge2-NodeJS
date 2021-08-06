40d0f5c48b01623e91f446c9ce62ee9262357f7d
agusahumada <agustinaahumada649@gmail.com>
2021-08-06 01:45:39 -0300

    New controllers file

---
+++ controllers/userController.js
@@ -0,0 +1,100 @@
+const User = require("../models/User");
+const Joi = require("@hapi/joi");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const schemaRegister = Joi.object({
+    name: Joi.string().min(6).max(255).required(),
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required(),
+});
+  
+const schemaLogin = Joi.object({
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required(),
+});
+  
+
+exports.userLogin = async (req, res) => {
+    // validates
+    const { error } = schemaLogin.validate(req.body);
+    if (error) return res.status(400).json({ error: error.details[0].message });
+    const user = await User.findOne({ email: req.body.email });
+    if (!user)
+      return res
+        .status(400)
+        .json({ error: true, message: "Usuario no registrado" });
+  
+    const passValid = await bcrypt.compare(req.body.password, user.password);
+    if (!passValid)
+      return res
+        .status(400)
+        .json({ error: true, message: "Contraseña inválida" });
+  
+    const token = jwt.sign(
+      {
+        name: user.name,
+        id: user._id,
+      },
+      process.env.TOKEN_SECRET
+    );
+  
+    res.json({
+      error: null,
+      message: "Bienvenido",
+      token: token,
+    });
+}
+
+exports.userGreeting = async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user)
+      return res
+        .status(400)
+        .json({ error: true, message: "Usuario no registrado" });
+    const token = jwt.sign(
+      {
+        name: user.name,
+        id: user._id,
+      },
+      process.env.TOKEN_SECRET
+    );
+  
+    if (token) {
+      res.send(`Buenos días ${user.name} !`).status(200);
+    }
+}
+
+exports.userRegister = async (req, res) => {
+  //validate user
+  const { error } = schemaRegister.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+
+  const emailExists = await User.findOne({ email: req.body.email });
+  if (emailExists)
+    return res.status(400).json({
+      error: true,
+      message: "El email ingresado ya está registrado",
+    });
+
+  //hash password
+  const salt = await bcrypt.genSalt(10);
+  const password = await bcrypt.hash(req.body.password, salt);
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: password,
+  });
+  try {
+    const userDB = await user.save();
+    res.json({
+      error: null,
+      data: userDB,
+    });
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,74 +1,10 @@
 const router = require("express").Router();
-const User = require("../models/User");
-const Joi = require("@hapi/joi");
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const userController = require("../controllers/userController");
 
-const schemaRegister = Joi.object({
-  name: Joi.string().min(6).max(255).required(),
-  email: Joi.string().min(6).max(255).required().email(),
-  password: Joi.string().min(6).max(1024).required(),
-});
+router.post("/login", userController.userLogin);
 
-const schemaLogin = Joi.object({
-    email: Joi.string().min(6).max(255).required().email(),
-    password: Joi.string().min(6).max(1024).required()
-})
+router.get("/greeting", userController.userGreeting);
 
-router.post('/login', async (req,res) => {
-     // validates
-     const { error } = schemaLogin.validate(req.body);
-     if (error) return res.status(400).json({ error: error.details[0].message }) 
-     const user = await User.findOne({email: req.body.email});
-     if(!user) return res.status(400).json({error: true, message: 'Usuario no registrado'});
-
-     const passValid = await bcrypt.compare(req.body.password, user.password); 
-     if(!passValid) return res.status(400).json({error: true, message: 'Contraseña inválida'});
-
-     const token = jwt.sign({
-         name: user.name,
-         id: user._id
-     }, process.env.TOKEN_SECRET)
-
-     res.json({
-         error: null,
-         message: 'Bienvenido',
-         token: token
-     })
-
-})
-
-router.post("/register", async (req, res) => {
-
-  //validate user
-  const { error } = schemaRegister.validate(req.body);
-  if (error) {
-    return res.status(400).json({error: error.details[0].message,});
-  }
-
-  const emailExists = await User.findOne({email: req.body.email});
-  if(emailExists) return res.status(400).json({
-    error: true, message: 'El email ingresado ya está registrado'
-  });
-
-  //hash password
-  const salt = await bcrypt.genSalt(10);
-  const password = await bcrypt.hash(req.body.password, salt);
-
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: password
-  });
-  try {
-    const userDB = await user.save();
-    res.json({
-      error: null,
-      data: userDB,
-    });
-  } catch (error) {
-    res.status(400).json({ error });
-  }
-});
+router.post("/register", userController.userRegister);
 
 module.exports = router;
