fe83c1930794301c902b3ab5813ad59de6ff843f
agusahumada <agustinaahumada649@gmail.com>
2021-08-05 23:28:55 -0300

    Login with JWT

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/User");
 const Joi = require("@hapi/joi");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const schemaRegister = Joi.object({
   name: Joi.string().min(6).max(255).required(),
@@ -9,19 +10,45 @@ const schemaRegister = Joi.object({
   password: Joi.string().min(6).max(1024).required(),
 });
 
+const schemaLogin = Joi.object({
+    email: Joi.string().min(6).max(255).required().email(),
+    password: Joi.string().min(6).max(1024).required()
+})
+
+router.post('/login', async (req,res) => {
+     // validates
+     const { error } = schemaLogin.validate(req.body);
+     if (error) return res.status(400).json({ error: error.details[0].message }) 
+     const user = await User.findOne({email: req.body.email});
+     if(!user) return res.status(400).json({error: true, message: 'Usuario no registrado'});
+
+     const passValid = await bcrypt.compare(req.body.password, user.password); 
+     if(!passValid) return res.status(400).json({error: true, message: 'Contraseña inválida'});
+
+     const token = jwt.sign({
+         name: user.name,
+         id: user._id
+     }, process.env.TOKEN_SECRET)
+
+     res.json({
+         error: null,
+         message: 'Bienvenido',
+         token: token
+     })
+
+})
+
 router.post("/register", async (req, res) => {
 
   //validate user
   const { error } = schemaRegister.validate(req.body);
   if (error) {
-    return res.status(400).json({
-      error: error.details[0].message,
-    });
+    return res.status(400).json({error: error.details[0].message,});
   }
 
   const emailExists = await User.findOne({email: req.body.email});
   if(emailExists) return res.status(400).json({
-    error: true, message: 'email already registered'
+    error: true, message: 'El email ingresado ya está registrado'
   });
 
   //hash password
